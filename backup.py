a9118340ecb7047d35e2684bd1502724eac3b6e4
chungthuang <7826979+chungthuang@users.noreply.github.com>
2020-09-06 15:31:46 +0100

    List returns a json with files as root key

--- backup.py
+++ backup.py
@@ -60,7 +60,7 @@ def remove_old_folders(drive_service):
         no_older_than.strftime("%Y-%m-%dT-%H:%M"), FOLDER_MIME, os.environ['ROOT_FOLDER_ID'])
     files = drive_service.files().list(
         spaces='drive', fields='files(name, id)', q=q).execute()
-    for f in files:
+    for f in files.get('files'):
         drive_service.files().delete(fileId=f.get('id')).execute()
         print("deleted {}".format(f.get('name')))
 
